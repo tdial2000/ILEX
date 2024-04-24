9919aad82ef2306ae6af50282724da70bb9b26de
tdial2000 <tysondial2000@hotmail.com>
2024-04-24 12:23:32 +1000

    Not important

--- test_script.py
+++ test_script.py
@@ -1,50 +1,25 @@
 from ilex.frb import FRB
 from ilex.data import *
-from ilex.plot import *
+from ilex.plot import _PLOT
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
 
 
-frb = FRB(name = "FRB220610", cfreq = 1271.5, bw = 336, dt = 50e-3, df = 4, t_crop = [20.0, 24.5],
-            f_crop = [1103.5, 1200], verbose = True, terr_crop = [0.0, 15])
-frb.load_data(ds_I = "examples/220610_dsI.npy")
+x = np.linspace(2150, 2250, 250)
 
-print(frb.par)
+y = np.cos(x)
 
-frb.fit_tscatt(npulse = 1, priors = {'a1': [0.5, 0.8], 'mu1': [21.0, 22.0], 'sig1': [0.1, 1.0], 'tau': [0.01, 2.0]})
+yerr = 0.05 * np.ones(250)
 
-sys.exit()
-
-# lets make a simple scalar weight that multiplies the samples in time
-# by -1 so we can see it works
-# lets plot the before and after 
-frb.plot_data("fI", filename = "docs/source/Tutorials/spec_before_W.png")     # before
-
-frb.par.tW.set(W = -1, method = "None")
-frb.plot_data("fI", filename = "docs/source/Tutorials/spec_after_W.png")     # after
-# NOTE: the None method is used to specify we want to take the values weights.W as 
-# the weights
+_PLOT(x = x, y = y, yerr = yerr, color = 'r', plot_type = "scatter")
 
 
-# define a weighting function, we will make use of a constrained boxcar filter.
-# def mask_rms(x, minval, maxval):
-#     """
-#     Mask data based on rms
-#     """
+plt.show()
+# frb = FRB(yaml_file = "examples/220610.yaml")
 
-#     W = np.ones(x.size)
-
-#     # set anything before minval to zero
-#     W[x < minval] = 0.0
-
-#     # set anything after maxval to zero
-#     W[x > maxval] = 0.0
-
-#     return W
-
-# # define the values of minval and maxval to use
-# frb.par.tW
+# frb.plot_data("tI")
+# frb.plot_stokes(stk_type = "t", stk_ratio = True, plot_type = "scatter", sigma = 10.0)
 
 
 

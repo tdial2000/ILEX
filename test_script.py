5330ba104f65efccab82e391753f502b78eb0776
tdial2000 <tysondial2000@hotmail.com>
2024-08-28 12:03:53 +1000

    updates

--- test_script.py
+++ test_script.py
@@ -2,31 +2,31 @@ from ilex.frb import FRB
 from ilex.data import *
 from ilex.plot import _PLOT
 import matplotlib.pyplot as plt
+from ilex.fitting import make_scatt_pulse_profile_func
 import numpy as np
 import sys
 
+from ilex.logging import break_str, log_title, set_verbose
 
-frb = FRB(yaml_file = "/fred/oz002/tdial/HTR_paper_data/230708/230708_updated.yaml")
 
-# data1 = frb.get_data(['tI'], get = True, apply_tW = False, t_crop = [1731.29, 1757.73])
-data2 = frb.get_data(['tI'], get = True, t_crop = [1731.292, 1734.014], apply_tW = False)
-# data3 = frb.get_data(['tI'], get = True, t_crop = [1731.292, 1734.014], apply_tW = False, tN = 30)
-# data2 = frb.get_data(['tI'], t_crop = [1737.291, 1744.345], get = True, apply_tW = False)
-# data3 = frb.get_data(['tI'], t_crop = [1741.432452151, 1746.5642356235], get = True, apply_tW = False)
+frb = FRB(yaml_file = "/fred/oz002/tdial/HTR_paper_data/230708/230708_updated.yaml", name = "Testing plots")
+frb.set(verbose = False)
 
-# print(data2['time'])
-# print(data3['time'])
+t = {}
+for i in [1, 10, 20, 50]:
+    dat = frb.get_data("fI", get = True, tN = i, fN = 1, t_crop = [1734, 1734.15])
+    t[f"{i}"], t[f"{i}err"] = dat['fI'].copy(), dat['fIerr'].copy()
 
-# print(frb)
 
-# plt.figure(figsize = (10,10))
+fig, ax = plt.subplots(1, 1, figsize = (10,10))
 
-# plt.plot(data1['time'], data1['tI'], alpha = 0.3)
-# plt.plot(data2['time'], data2['tI'], alpha = 0.3)
-# plt.plot(data3['time'], data3['tI'], alpha = 0.3)
+for i in [1, 10, 20, 50]:
+    _PLOT(dat['freq'], t[f"{i}"], t[f"{i}err"], ax = ax, label = f"tN = {i}")
 
-# print(data1['time'])
-print(data2['time'])
-# print(data3['time'])
+plt.legend()
+plt.show()
 
-# plt.show()
+
+# frb.set(RM = frb.fitted_params['RM']['rm'].val)
+
+# frb.plot_PA()

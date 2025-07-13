fixed data weighting with freq scrunching

ilex/data.py
@@ -145,6 +145,75 @@ def scrunch(x: np.ndarray, axis: int = 0, weights = None, nan = False):
 
 
 
+def downsample(x: np.ndarray, axis: int = -1, N: int = 1, nan = False, 
+                mode = "mean"):
+    """
+    Downsample in either frequency or time
+
+    Parameters
+    ----------
+    x: ndarray
+       data to average over
+    axis: int 
+        axis to average over
+    N: int
+        donwsampling factor
+    nan : bool, optional
+        If True, using nanmean to ignore NaN values in array 'x', by default False
+    mode : str
+        Method of downsampling, ["mean", "sum"]
+
+    Returns
+    -------
+    x: ndarray 
+       Averaged data
+    
+    """
+
+    # if nan if true, will use numpy function that ignores nans in array x
+    if mode == "mean":
+        func = np.mean
+        if nan:
+            func = np.nanmean
+    elif mode == "sum":
+        func = np.sum
+        if nan:
+            func = np.nansum
+    else:
+        ValueError(f"Downsampling mode must be either ['mean', 'sum']")
+
+
+    if N == 1:
+        return x
+    
+
+    # either dynamic spectra or time series
+    ndims = x.ndim
+    if ndims == 1:
+        N_new = int(x.size / N) * N
+        return func(x[:N_new].reshape(int(N_new / N), N),axis = 1).flatten()
+    
+    elif ndims == 2:
+        if axis == 0:
+            #frequency scrunching
+            N_new = int(x.shape[0] / N) * N
+            return func(x[:N_new].T.reshape(x.shape[1],int(N_new / N), N),axis = 2).T
+        
+        elif axis == 1 or axis == -1:
+            #time scrunching
+            N_new = int(x.shape[1] / N) * N
+            return func(x[:,:N_new].reshape(x.shape[0],int(N_new / N), N),axis = 2)
+        
+        else:
+            print("axis must be 1[-1] or 0")
+            return x
+        
+    else:
+        print("ndims must equal 1 or 2..")
+        return x
+
+
+
 
 ##  function to index in phase  ##
 def pslice(x: np.ndarray, start: float, end: float, axis: int = 0):
@@ -640,7 +709,7 @@ def combine_zapchan(chan1, chan2):
 ##===============================================##
 
 
-def calc_PA(Q, U, Qerr, Uerr, rad2deg = False):
+def calc_PA(Q, U, Qerr = None, Uerr = None, rad2deg = False):
     """
     Calculate Position Angle (PA) and PA angle
 
@@ -665,15 +734,22 @@ def calc_PA(Q, U, Qerr, Uerr, rad2deg = False):
         Position Angle err  
     """
 
+    errflag = True
+    PAerr = None
+    if (Qerr is None) or (Uerr is None):
+        errflag = False
+
     # calculate PA and error
     PA = 0.5 * np.arctan2(U, Q)
-    PAerr = 0.5 * np.sqrt((Q**2*Uerr**2 + Q**2*Qerr**2)/
+    if errflag:
+        PAerr = 0.5 * np.sqrt((Q**2*Uerr**2 + Q**2*Qerr**2)/
                            (Q**2 + Q**2)**2)
     
     # convert to degrees if requested
     if rad2deg:
         PA *= 180/np.pi
-        PAerr *= 180/np.pi
+        if errflag:
+            PAerr *= 180/np.pi
 
 
     return PA, PAerr
@@ -1188,3 +1264,48 @@ def ccf(x, y, outs = "unique"):
 
     else:
         return None
+
+
+
+def unwrap_pa(f, pa, rm, pa0):
+    """
+    Unwrap polarisation position angle (PA) with a given RM 
+    
+    """
+    if rm == 0.0:
+        ValueError("RM is 0, cannot unwrap...")
+    def rmquad(f, rm, pa0):
+        angs = rm*c**2/1e12*(1/f**2)
+        return 180/np.pi*angs
+    
+    # calculate number of points needed for PA model
+    f1 = f[-1]
+    rmconst1 = 180 * rm * c**2 / (np.pi * 1e12) - f1**2
+    rmconst2 = 2 * f1 * 180 * rm * c**2 / (np.pi * 1e12) - 2*f1**3
+    rmconst3 = f1**4
+
+    rmsgn = rm/abs(rm)
+
+    eps = (-rmconst2 + rmsgn * np.sqrt(rmconst2**2 + 4*rmconst1*rmconst3)) / (2 * rmconst1)
+    df = abs(f[-1] - f[-2])
+    bw = (f[0] - f[-1] + df)
+    N = int(bw / abs(eps))
+
+    # get model data
+    fmodel = np.linspace(f[0] + df/2, f[-1] - df/2, N)
+    pamodel = rmquad(fmodel, rm, pa0)
+    pamodel_wrapped = pamodel % 180
+    wrap_points = np.where(np.abs(np.diff(pamodel_wrapped)) > 10)[0] + 1
+    
+    wrap_points = (wrap_points / (pamodel.size - 1) * (f.size - 1)).astype(int)
+    pa_unwrapped = pa.copy()
+    idx = 0 
+    i = 0
+    for i in range(wrap_points.size):
+        pa_unwrapped[idx:wrap_points[i]] += float(rmsgn * i * 180.0)
+        idx = wrap_points[i]
+    pa_unwrapped[idx:] += float(rmsgn * (i+1)*180)
+    pa_unwrapped -= (pa_unwrapped[-1] - rmquad(f, rm, pa0)[-1])
+    
+
+    return pa_unwrapped, fmodel, pamodel

ilex/frb.py
@@ -1504,6 +1504,7 @@ class FRB:
         ## check if data valid##
         ##====================## 
 
+        # if 't_crop' not in kwargs.keys():
         kwargs['t_crop'] = ["min", "max"]
         # kwargs['f_crop'] = ["min", "max"]  
         kwargs['terr_crop'] = None
@@ -1545,6 +1546,7 @@ class FRB:
         
 
         # now choose method of finding burst bounds
+        log(f"Searching for Width using: {method} method", lpf_col=self.pcol)
         if method == "sigma":
             ms2phase = lambda x : x / (tI.size * self.this_par.dt)
             ref_ind, lw, rw = find_optimal_sigma_width(tI = tI, sigma = sigma,
@@ -2341,7 +2343,7 @@ class FRB:
 
 
     def fit_RM(self, method = "RMquad", sigma: float = None, rm_prior: list = [-1000, 1000], 
-                pa0_prior: list = [-3.1415926/2, 3.1415926/2], fit_params: dict = None, filename: str = None, **kwargs):
+                pa0_prior: list = [-3.1415926/2, 3.1415926/2], unwrap: bool = False, fit_params: dict = None, filename: str = None, **kwargs):
         """
         Fit Spectra for Rotation Measure
 
@@ -2444,6 +2446,10 @@ class FRB:
         def rmquad(f, rm, pa0):
             angs = pa0 + rm*c**2/1e12*(1/f**2 - 1/f0**2)
             return 90/np.pi*np.arctan2(np.sin(2*angs), np.cos(2*angs))
+        
+        def rmquad_unwrapped(f, rm, pa0):
+            angs = pa0 + rm*c**2/1e12*(1/f**2)
+            return 180/np.pi * angs
 
 
         # put into pyfit structure
@@ -2466,8 +2472,23 @@ class FRB:
 
         # plot
         if self.save_plots or self.show_plots:
+
+            pa_ylim = [-90, 90]
+            if unwrap:
+                y_wrap = p.y.copy()
+                func_wrap = p.func
+                y_unwrap, _, _ = unwrap_pa(p.x, p.y, rm, pa0)
+                p.set(y = y_unwrap, func = rmquad_unwrapped)
+                y_height = np.max(y_unwrap) - np.min(y_unwrap)
+                pa_ylim = [np.min(y_unwrap) - 0.15*y_height, np.max(y_unwrap) + 0.15*y_height]
             
-            p.plot(xlabel = "Frequency [MHz]", ylabel = "PA [deg]", ylim = [-90, 90], show = False)
+            fig = p.plot(xlabel = "Frequency [MHz]", ylabel = "PA [deg]", ylim = pa_ylim, show = False)
+
+            if unwrap:
+                p.set(y = y_wrap, func = func_wrap)
+                if self.residuals:
+                    fig.axes[1].set(ylim = [-360, 360])
+
 
             if self.save_plots:
                 if filename is None:
@@ -2825,38 +2846,6 @@ class FRB:
         
 
         return fracS
-
-
-
-
-    # def save_crop(self, buffer = 1.2, **kwargs):
-    #     """
-    #     Save just a crop of the data and a modified ilex config file with updated crop params
-
-    #     Parameters
-    #     ----------
-    #     buffer: float
-    #         Amount of padding to put on either side of on-pulse (+ off-pulse) window when
-    #         making new crop.
-
-    #     """
-
-    #     # proc KWARGS
-    #     self._load_new_params(**kwargs) 
-
-    #     stk = []
-    #     for s in "IQUV":
-    #         if self.ds[s] is not None:
-    #             stk += [f"ds{s}"]
-
-
-    #     # make larger crop by taking into account both on-pulse and off-pulse windows
-    #     t_width = 
-    #     t_crop_full = [self.this_metapar.t_crop[0]]
-
-    #     # get data
-    #     # force disable all processing other than cropping
-    #     data = self.get_data(stk, get = True, tN = 1, fN = 1, RM = None, zapchan = "", )
 
 
 

ilex/interactive.py
@@ -172,6 +172,9 @@ class _MouseZapper:
         self.last_key_event = None
         self.last_key_press = time.time()
 
+        self._list_of_auto_alg = ["median", "abs"]
+        self._auto_alg = "median"
+
 
         # artists
         self._artists = {'ds': None, 'zapatch': None, 
@@ -460,16 +463,17 @@ class _MouseZapper:
     
         if event.key == "a":
             # do auto flagging
-            print("hi")
             if not self.zapmode:
                 return
+            
+            print("automated flagging using method: " + self._auto_alg)
 
             with warnings.catch_warnings():
                 warnings.simplefilter("ignore")
-                zap_threshold = input("-[Input]: Sigma threshold for automated Frequency zapping-\n")
+                zap_threshold = input("-[Input]: Value for automated Frequency zapping-\n")
             try:
                 zap_threshold = float(zap_threshold)
-                print(f"Zapping with a sigma threshold of {zap_threshold}")
+                print(f"Zapping with a value of {zap_threshold}")
             except:
                 print(f"{zap_threshold} is not a valid floating point number!!!")
                 return
@@ -481,11 +485,33 @@ class _MouseZapper:
 
             self.zaps.append(zaps)
 
+            # update time series and freq axes
+
             # update figure
             self._update_ax()
-
+            self.ax.set_xlim(self.ax.get_xlim())
             
             return
+        
+
+        if event.key == "x":
+            if not self.zapmode:
+                return
+            
+            # change automatic flagging method
+            idx = self._list_of_auto_alg.index(self._auto_alg)
+            if idx == len(self._list_of_auto_alg) - 1:
+                idx = 0
+            else:
+                idx += 1
+            
+            self._auto_alg = self._list_of_auto_alg[idx]
+
+            print("Changed automated flagging method to: " + self._auto_alg)
+
+            return
+
+
 
         if (event.key in ['up', 'down']) and (self._last_artist is not None) and (self.zapmode):
             if event.key == 'up':
@@ -500,7 +526,7 @@ class _MouseZapper:
             self.ax.set_xlim(self.ax.get_xlim())      # this is only here to make sure the figure updates properly
             self.fig.canvas.draw()
 
-
+            return
         
         return
     
@@ -521,6 +547,10 @@ class _MouseZapper:
         ylim[0] = 1.0 - (ylim[0] - ybounds[0])/bw
         ylim[1] = 1.0 - (ylim[1] - ybounds[0])/bw
 
+        tw = self.xlim[1] - self.xlim[0]
+        xlim[0] = (xlim[0] - self.xlim[0])/tw
+        xlim[1] = (xlim[1] - self.xlim[0])/tw
+
         ylim = ylim[::-1]
 
         ds_crop = self.zap_ds.copy()
@@ -530,18 +560,20 @@ class _MouseZapper:
         ds_crop = pslice(ds_crop, *ylim, axis = 0)
         freq_crop = pslice(self.freqs, *ylim)
 
-        # with cropped data, apply zapping algorithm
-        # f_std = np.nanstd(ds_crop, axis = 1)
-        # med_rms = np.nanmedian(f_std)
-        # mad_rms = 1.48 * np.nanmedian(np.abs(f_std - med_rms))
-
-        # zaps = np.where(f_std > (med_rms + zap_threshold * mad_rms))[0]
-
+        # do automated flagging
         mask = np.mean(ds_crop, axis = 1)
+
         with warnings.catch_warnings():
             warnings.simplefilter("ignore")
-            mask /= np.nanmedian(mask)
-        zaps = np.where((mask > zap_threshold) & (~np.isnan(mask)))[0]
+            if self._auto_alg == "median":
+                median = np.nanmedian(mask)
+                mask /= median
+                print(f"Median: {median:.2f}")
+                zaps = np.where((mask > zap_threshold) & (~np.isnan(mask)))[0]
+
+
+            elif self._auto_alg == "abs":
+                zaps = np.where(mask > zap_threshold)[0]
 
         if len(zaps) < 1:
             return None
@@ -558,6 +590,15 @@ class _MouseZapper:
         to display zapped regions
         
         """
+        # if xlim and ylim change callbacks are defined!
+        # if hasattr(self.ax, 'callbacks'):
+        #     _callbacks = self.ax.callbacks.callbacks
+        #     if "xlim_changed" in _callbacks.keys():
+        #         self.ax.set_xlim(self.ax.get_xlim())
+        #     if "ylim_changed" in _callbacks.keys():
+        #         self.ax.set_ylim(self.ax.get_ylim())
+
+
         self.zap_ds, self.zap_idx = self._zap_ds()
         mask = np.ones(self.zap_ds.shape[0], dtype = bool)
         mask[self.zap_idx] = False
@@ -699,6 +740,7 @@ def ZapInteractive(ds, freqs, times = None, zapchan = None):
             zapzoom.update_zoom()
 
     def _draw_freq_lines(event):
+
 
         freq_lines = {}
         freq_line_labels = []

ilex/master_proc.py
@@ -2,6 +2,7 @@
 from .data import *
 from .logging import log, get_verbose
 from .utils import get_stk_from_datalist
+from .frbutils import dynspec_smart_loader
 from .globals import _G
 from time import time
 import matplotlib.pyplot as plt
@@ -134,25 +135,39 @@ def master_proc_data(stk, freq, base_data_list, par: dict = {}, debias = False, 
     ##         RUN PROCESSING         ##
     ## ============================== ##
     # log("Processing data", lpf = False)
+    # also crop/average freq array
+    freq = pslice(freq, *par['f_crop'])    
+
+
+    # # check if zap_chan flag exists
+    # seg_idx = zap_chan(freq, par['zapchan'])
+    # if len(seg_idx) == 0:
+    #     zap_flag = False
 
 
     ## crop ##    
     log(f"Applying On-Pulse Time Crop = {par['t_crop']}", lpf = False)
     log(f"Applying Off-Pulse Time crop = {par['terr_crop']}", lpf = False)
     log(f"Applying Freq Crop = {par['f_crop']}", lpf = False)
-    stk_ds = _crop(stk = stk, stk_ds = stk_ds, stk_list = stk_list, 
-                    par = par, err = err_flag)
+    log(f"Applying Time Averaging = {par['tN']}", lpf = False)
+    # stk_ds = _crop(stk = stk, stk_ds = stk_ds, stk_list = stk_list, 
+    #                 par = par, err = err_flag)
 
-    # also crop freq array
-    freq = pslice(freq, *par['f_crop'])
+    fN = par['fN']
+    par['fN'] = 1
+    stk_ds = _master_dynspec_smart_load(stk = stk, stk_ds = stk_ds, stk_list = stk_list,
+                par = par, err = err_flag)
+    par['fN'] = fN
 
 
 
     ## Channel Zap ##
+    freqscrunchw = np.ones(freq.size, dtype = float)
     if zap_flag:
         log(f"Applying channel zapping", lpf = False)
-        stk_ds, zap_flag = _zap_chan(stk = stk, stk_ds = stk_ds, stk_list = stk_list,
+        stk_ds, zap_flag, flagchans = _zap_chan(stk = stk, stk_ds = stk_ds, stk_list = stk_list,
                                 freq = freq, par = par, err = err_flag)
+        freqscrunchw[flagchans] = np.nan
         
         if not zap_flag:
             log("All channel zapping lies outside the known bandwidth, will skip channel zapping", lpf = False, stype = "warn")
@@ -171,16 +186,20 @@ def master_proc_data(stk, freq, base_data_list, par: dict = {}, debias = False, 
 
     
     # ## average ##
-    log(f"Applying Time Averaging = {par['tN']}", lpf = False)
     log(f"Applying Freq Averaging = {par['fN']}", lpf = False)
+    tN = par['tN']
+    par['tN'] = 1    
     stk_ds = _average(stk = stk, stk_ds = stk_ds, stk_list = stk_list,
                     par = par, err = err_flag, zap = zap_flag)
+    par['tN'] = tN
 
+    freq = average(freq, N = par['fN'])  
 
-
-    # also average freq array
-    freq = average(freq, N = par['fN'], nan = zap_flag)
-
+    # create set of freq weights to apply when scrunching to properly scale the noise when channels
+    # are zapped
+    freqscrunchw = downsample(freqscrunchw, N = par['fN'], nan = zap_flag, mode = "sum")
+    freqscrunchw = np.sqrt(freqscrunchw) 
+    freqscrunchw /= np.max(freqscrunchw) 
 
     ## ========================= ##
     ## SAVING DATA TO CONTAINERS ##
@@ -213,7 +232,14 @@ def master_proc_data(stk, freq, base_data_list, par: dict = {}, debias = False, 
 
         #------- time series -------#
         if S in t_list:
-            _t[S] = _scrunch(stk_ds[S], xtype = "t", W = _average_weight(par['fW'], par['fN']), nan = zap_flag)
+            fweights = _average_weight(par['fW'], par['fN'])
+            if par['fN'] > 1:
+                if fweights is not None:
+                    fweights *= freqscrunchw
+                else:
+                    fweights = freqscrunchw
+
+            _t[S] = _scrunch(stk_ds[S], xtype = "t", W = fweights, nan = zap_flag)
             if err_flag:
                 _t[f"{S}err"] = _time_err(stk_ds[f"{S}err"], nan = zap_flag)
 
@@ -254,6 +280,20 @@ def master_proc_data(stk, freq, base_data_list, par: dict = {}, debias = False, 
 
 
 
+
+def _master_dynspec_smart_load(stk, stk_ds, stk_list, par, err):
+
+    for S in stk_list:
+        print("SMART LOADING STK: "+S)
+        stk_ds[S] = dynspec_smart_loader(stk[S], par['t_crop'], par['f_crop'],
+                                         par['tN'], 1)
+        if err:
+            stk_ds[f"{S}err"] = dynspec_smart_loader(stk[S], par['terr_crop'], par['f_crop'],
+                                         par['tN'], 1)
+    
+    return stk_ds
+
+        
 
 
 
@@ -362,7 +402,7 @@ def _crop(stk, stk_ds, stk_list, par, err = False):
         Full crop in Time and Frequency
 
     Args:
-        See Master _proc
+        See master_proc
     """
 
     for S in stk_list:
@@ -603,76 +643,10 @@ def _zap_chan(stk, stk_ds, stk_list, freq, par, err = False):
     """
     seg_idx = zap_chan(freq, par['zapchan'])
 
-    # # vals
-    # df = freq[1] - freq[0]
-    # f_min = np.min(freq)
-    # f_max = np.max(freq)
-
-    # if df < 0:
-    #     # upperside band
-    #     fi = f_max
-    #     df_step = -1
-
-    # else:
-    #     # lowerside band
-    #     fi = f_min
-    #     df_step = 1
-
-    # df = abs(df)
-    
-    # # split segments
-    # zap_segments = par['zapchan'].split(',')
-    # seg_idx = []
-
-    # # for each segment, check for delimiter :, else float cast
-    # for _, zap_seg in enumerate(zap_segments):
-
-    #     # if segment is a range of frequencies
-    #     if ":" in zap_seg:
-    #         zap_range = zap_seg.strip().split(':')
-    #         zap_0 = round(df_step * (float(zap_range[0]) - fi)/df)
-    #         zap_1 = round(df_step * (float(zap_range[1]) - fi)/df)
-
-
-    #         # check if completely outside bounds
-    #         if (zap_0 < 0 and zap_1 < 0) or (zap_0 > freq.size -1 and zap_1 > freq.size -1):
-    #             log(f"zap range [{zap_range[0]}, {zap_range[1]}] MHz out of range of bandwidth [{f_min}, {f_max}] MHz", lpf = False, stype = "warn")
-    #             continue            
-            
-    #         # check bounds
-    #         crop_zap = False
-
-    #         if zap_0 < 0:
-    #             crop_zap = True
-    #             zap_0 = 0
-    #         elif zap_0 > freq.size - 1:
-    #             crop_zap = True
-    #             zap_0 = freq.size - 1
-
-    #         if zap_1 < 0:
-    #             crop_zap = True
-    #             zap_1 = 0
-    #         elif zap_1 > freq.size - 1:
-    #             crop_zap = True
-    #             zap_1 = freq.size - 1
-
-    #         if crop_zap:
-    #             log(f"zap range cropped from [{zap_range[0]}, {zap_range[1]}] MHz -> [{freq[zap_0]}, {freq[zap_1]}] MHz", lpf = False, stype = "warn")
-
-    #         seg_idx += list(range(zap_0,zap_1+df_step,df_step))[::df_step]
-        
-    #     # if segment is just a single frequency
-    #     else:
-    #         _idx = round(df_step * (float(zap_seg.strip()) - fi)/df)
-    #         if (_idx < 0) or (_idx > freq.size - 1):
-    #             log(f"zap channel {zap_seg.strip()} MHz out of bounds of bandwidth [{f_min}, {f_max}] MHz", lpf = False, stype = "warn")
-    #         else:
-    #             seg_idx += [_idx]
-
 
     # check if seg_idx is empty, i.e. if there is no channel zapping.
     if len(seg_idx) == 0:
-        return stk_ds, False
+        return stk_ds, False, seg_idx
 
 
     # mask out zapped channels
@@ -682,7 +656,7 @@ def _zap_chan(stk, stk_ds, stk_list, freq, par, err = False):
         if err:
             stk_ds[f"{S}err"][seg_idx] = np.nan
 
-    return stk_ds, True
+    return stk_ds, True, seg_idx
 
 
 

ilex/plot.py
@@ -96,6 +96,11 @@ def _data_from_dict(dic, keys):
 
 
 
+# wrapper for better function definition
+def plot(x, y, yerr = None, ax = None, plot_type = "lines", color = None, alpha = 0.5, **kwargs):
+    _PLOT(x = x, y = y, yerr = yerr, ax = ax, plot_type = plot_type, color = color,
+            alpha = alpha, **kwargs)
+    return
 
 
 
@@ -351,7 +356,7 @@ def plot_data(dat, typ = "dsI", ax = None, plot_type = "scatter"):
     return fig
 
 
-
+# def plot_RM_fit(f, pa, rm, pa0, f0, residual = False):
 
 
 def plot_RM(f, Q, U, Qerr = None, Uerr = None, rm = 0.0, pa0 = 0.0, f0 = 0.0,
@@ -522,11 +527,14 @@ def plot_PA(x, PA, PA_err, ax = None, flipPA = False,
     # ax.scatter(x, PA * 180/np.pi, c = 'k', s = 2)
     _PLOT(x = x, y = PA * 180/np.pi, yerr = PA_err * 180/np.pi,
              plot_type = plot_type, ax = ax, **kwargs)
+    
+    paw = np.nanmax(PA * 180/np.pi) - np.nanmin(PA * 180/np.pi)
+    ax.set_ylim([np.nanmin((PA - PA_err) * 180/np.pi) - 0.1*paw, np.nanmax((PA + PA_err) * 180/np.pi) + 0.1*paw])
 
-    if flipPA:
-        ax.set_ylim([0, 180])
-    else:
-        ax.set_ylim([-90, 90])
+    # if flipPA:
+    #     ax.set_ylim([0, 180])
+    # else:
+    #     ax.set_ylim([-90, 90])
 
 
     return fig

ilex/pyfit.py
@@ -430,6 +430,7 @@ class fit:
         self._is_fit = False
         self.residuals = True
         self.plotPosterior = True
+        self.modelNpoints = 20000
 
 
 
@@ -1449,15 +1450,29 @@ class fit:
         AX[0].scatter(self.x, self.y, c = 'k', s = 10)
 
         # plot model
+        Mx = x = np.linspace(self.x[0], self.x[-1], self.modelNpoints)
+        _, Mline = self.get_model(x = Mx)
         _, y_fit = self.get_model()
         if y_fit is None:
             return
-        AX[0].plot(self.x, y_fit, color = [0.9098, 0.364, 0.3961], linewidth = 2.5)
+        AX[0].plot(Mx, Mline, color = [0.9098, 0.364, 0.3961], linewidth = 2.5)
 
         # plot errorbars if specified
+        # axxerr, axyerr = self.xerr, self.yerr
         if (self.yerr is not None) or (self.xerr is not None):
             AX[0].errorbar(x = self.x, y = self.y, xerr = self.xerr, yerr = self.yerr, color = 'k', 
                         alpha = 0.4, linestyle = '')
+        
+        # if axxerr is None:
+        #     axxerr = 0
+        # if axyerr is None:
+        #     axyerr = 0
+            
+        # # set x, y limits
+        # ylimw = (np.max(self.y + axyerr) - np.min(self.y - axyerr))
+        # xlimw = (np.max(self.x + axxerr) - np.min(self.x - axxerr))
+        # AX[0].set_xlim([np.min(self.x - axxerr) - 0.1*xlimw, np.max(self.x + axxerr) + 0.1*xlimw])
+        # AX[0].set_ylim([np.min(self.y - axyerr) - 0.15*ylimw, np.max(self.y + axyerr) + 0.15*ylimw])
 
         # plot residuals
         if self.residuals:

ilex/script_core/plot_master.py
@@ -149,8 +149,8 @@ def _plot(args, figpar, flags):
 
 
     # create FRB instance
-    frb = FRB()
-    frb.load_data(yaml_file = args.parfile)
+    frb = FRB(args.parfile)
+    frb.set(show_plots = False, save_plots = False)
 
     # get data
     data_list = []
@@ -168,14 +168,26 @@ def _plot(args, figpar, flags):
     data = frb.get_data(data_list, get = True)
 
     pars = load_param_file(args.parfile)
+    print(data['tIerr'])
 
     if (args.model or flags['M']) and args.modelpar is None:
-        # run model
-        p = frb.fit_tscatt(method = pars['fits']['fitmethod'], npulse = pars['fits']['tscatt']['npulse'],
-            priors = pars['fits']['tscatt']['priors'], statics = pars['fits']['tscatt']['statics'],
-            fit_params = pars['fits']['tscatt']['fit_params'], redo = pars['fits']['redo'], 
-            filename = args.filename) 
-        NPULSES = pars['fits']['tscatt']['npulse']
+
+        if (pars['weights']['time']['func'] is not None) and (pars['weights']['time']['method'] == "func"):
+            NPULSES = (len(pars['weights']['time']['args'])-1) // 3
+            p = fit(x = data['time'], y = data['tI'], yerr = data['tIerr']*np.ones(data['tI'].size), 
+                    func = make_scatt_pulse_profile_func(NPULSES))
+            for key in pars['weights']['time']['args'].keys():
+                p.set_posterior(key, pars['weights']['time']['args'][key], 0.0, 0.0)
+            p._is_fit = True
+
+        else:
+
+            # run model
+            p = frb.fit_tscatt(method = pars['fits']['fitmethod'], npulse = pars['fits']['tscatt']['npulse'],
+                priors = pars['fits']['tscatt']['priors'], statics = pars['fits']['tscatt']['statics'],
+                fit_params = pars['fits']['tscatt']['fit_params'], redo = pars['fits']['redo'], 
+                filename = args.filename) 
+            NPULSES = pars['fits']['tscatt']['npulse']
     
     elif args.modelpar is not None:
         # create model
@@ -241,7 +253,7 @@ def _plot(args, figpar, flags):
         PA, PAerr = calc_PAdebiased(dict_get(data,["tU", "tQ", "tUerr", "tQerr", "tIerr"]), 
                         Ldebias_threshold = pars['plots']['Ldebias_threshold'])  
         plot_PA(data['time'], PA, PAerr, ax = AX['P'], flipPA = pars['plots']['flipPA'], 
-                plot_type = frb.plot_type)
+                plot_type = "scatter")
 
 
     # final figure adjustments